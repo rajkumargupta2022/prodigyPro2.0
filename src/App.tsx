6945441d496c418ec6f095be113ca3efa9b6a95f
rajkumargupta2022 <rajkumarbfcsofttech@gmail.com>
2025-02-05 11:42:24 +0530

    login first page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import "./assets/css/style.css"
+import "./assets/css/media.css"
 
 function App() {
   const [count, setCount] = useState<number>(0);
 
   return (
     <>
-       <Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,44 +1,36 @@
 import LeftImage from "../assets/img/leftLogin.png"
 import Logo from "../assets/img/logo/logo.png"
-const Login = ()=>{
-return (
+const Login = () => {
+  return (
 
-  <div className="container-fluid">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-6 col-12 hideImage">
+          <div className="leftLogin">
+            <img className="w-100 login_hight_fixed" src={LeftImage} alt="leftLogin" />
+          </div>
+        </div>
 
-<div className="row">
+        <div className="col-12 col-md-6 align-self-center">
+          <div className="mrgin_With20">
+            <img src={Logo} alt="" className="logoImage"/>
+            <p className="py-2">From BFC Capital Private Limited</p>
+            <form className="pt-3" action="">
+              <label className="form-label fontFamily fw-bold pb-1">Enter your phone number</label>
 
-    <div className="col-md-6">
-      <div className="leftLogin">
-      <img className="w-100 login_hight_fixed" src={LeftImage} alt="leftLogin" />
+              <div className="input-group mb-3">
+                <button className="btn rounded border" type="button" data-bs-toggle="dropdown" aria-expanded="false">+91</button>              
+                <input type="text" className="form-control mx-1 rounded" aria-label="Text input with dropdown button" placeholder="Phone Number" />
+              </div>
+              <button type="button" className="customButton col-12">Get OTP</button>
+            </form>
+            <p className="mt-3">By logging in, you agree to our Terms & Conditions</p>
+          </div>
+        </div>
       </div>
+
     </div>
 
-    <div className="col-md-6 align-self-center">
-      <div className="mrgin_With20">
-        <img src={Logo} alt="" />
-        <p className="py-2">From BFC Capital Private Limited</p>
-          <form className="pt-3" action=""> 
-            <label className="form-label fontFamily fw-bold pb-1">Enter your phone number</label>
-            
-            <div className="input-group mb-3">
-  <button className="btn rounded border" type="button" data-bs-toggle="dropdown" aria-expanded="false">+91</button>
-  <ul className="dropdown-menu">
-    <li><a className="dropdown-item" href="#">Action</a></li>
-    <li><a className="dropdown-item" href="#">Another action</a></li>
-    <li><a className="dropdown-item" href="#">Something else here</a></li>
-    <li><hr className="dropdown-divider"/></li>
-    <li><a className="dropdown-item" href="#">Separated link</a></li>
-  </ul>
-  <input type="text" className="form-control mx-1 rounded" aria-label="Text input with dropdown button" placeholder="Phone Number"/>
-</div>
-            <button type="button" className="customButton col-md-12 col-sd-12">Get otp</button>
-        </form>
-      </div>
-    </div>
-</div>
-
-  </div>
-
-)
+  )
 }
 export default Login
