3a4faffe4b41f14ad10cd1ed4e752d25665fb238
vishalsofttech <vishalbfcsofttech@gmail.com>
2025-02-06 13:25:23 +0530

    add bank details nomination details declaration address details

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import PersonalDetails from "./pages/Personal-details";
+import Declaration from "./pages/Declaration";
+import AddressDetails from "./pages/Address-details";
+import BankDetails from "./pages/Bank-Details";
+import NominationDetails from "./pages/Nomination-details";
 import "./assets/css/style.css";
 import "./assets/css/media.css";
 
@@ -13,6 +16,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/personal-details" element={<PersonalDetails />} />
+          <Route path="/declaration" element={<Declaration />} />
+          <Route path="/address-details" element={<AddressDetails />} />
+          <Route path="/bank-details" element={<BankDetails />} />
+          <Route path="/nomination-details" element={<NominationDetails />} />
         </Routes>
       </BrowserRouter>
     </>
