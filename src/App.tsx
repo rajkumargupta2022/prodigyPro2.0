51d51b91853e277ffdf2604c31295c1c7690df09
rajkumargupta2022 <rajkumarbfcsofttech@gmail.com>
2025-04-07 14:52:08 +0530

    working on marriage calculator

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ import RetirementCalculator from "./pages/calculator/RetirementCalculator";
 import EmiCalculator from "./pages/calculator/EmiCalculator";
 import FDCalculator from "./pages/calculator/FDCalculator";
 import ElssCalculator from "./pages/calculator/ElssCalculator";
-import SWPCalculator from "./pages/calculator/SWPCAlculator";
+import SWPCalculator from "./pages/calculator/SWPCalculator";
 import RecommendedSchemeGoal from "./pages/planning/RecommendedSchemeGoal";
 import RiskProfile from "./components/RiskProfile";
 import { ToastContainer } from "react-toastify";
