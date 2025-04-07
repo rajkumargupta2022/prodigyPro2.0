db4a349d60381e3382ab28783774441f1f88cdf3
vishalsofttech <vishalbfcsofttech@gmail.com>
2025-04-07 12:33:34 +0530

    working on education calculator

--- src/services/Validated-inputs/validations.tsx
+++ src/services/Validated-inputs/validations.tsx
@@ -1,4 +1,27 @@
 export const isNotEmpty = (value: string | number): string | null => {
-  if (value == "0") return "Field cannot be empty";
+  value = parseInt(value as string);
+
+  if (value <= 0) {
+    return "Enter valid input";
+  } else if (isNaN(value)) {
+    return "Field cannot be empty";
+  }
+  return null;
+};
+
+export const maxValue = (value: string | number): string | null => {
+  value = parseInt(value as string);
+  if (value > 100000000) {
+    return "Value Should be less then 100000000";
+  }
+  return null;
+};
+
+export const minAmount = (value: string | number): string | null => {
+  value = parseInt(value as string);
+
+  if (value < 1000) {
+    return "Amount should be greater then 1000";
+  }
   return null;
 };
