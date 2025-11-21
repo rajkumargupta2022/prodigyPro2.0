b9dd35c6c598e2567c54931df51a65e2676bbe33
Divyansh <divyanshbfc@gmail.com>
2025-11-21 12:58:34 +0530

    Fixed Loader

--- src/services/Api/AxiosInstance.ts
+++ src/services/Api/AxiosInstance.ts
@@ -1,17 +1,29 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { baseUrl } from "../utils/urls";
 import { showLoader, hideLoader } from "../Loader/LoaderController";
 
+let activeRequests = 0;
+
 const AxiosInstance = axios.create({
   baseURL: baseUrl,
+  timeout: 20000,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
 
 AxiosInstance.interceptors.request.use(
   (config) => {
-    showLoader();
-    const token = localStorage.getItem("token"); // Use your token key here
+    const shouldShow = (config as any).showLoader !== false;
+    if (shouldShow) {
+      activeRequests += 1;
+      if (activeRequests === 1) showLoader();
+    }
+    const token = localStorage.getItem("token");
     if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
+      config.headers = config.headers ?? {};
+      (config.headers as any)["Authorization"] = `Bearer ${token}`;
     }
     return config;
   },
@@ -23,12 +35,20 @@ AxiosInstance.interceptors.request.use(
 
 AxiosInstance.interceptors.response.use(
   (response) => {
-    // Hide loader when response received
-    hideLoader();
+    const shouldShow = (response.config as any).showLoader !== false;
+    if (shouldShow) {
+      activeRequests = Math.max(0, activeRequests - 1);
+      if (activeRequests === 0) hideLoader();
+    }
     return response;
   },
-  (error) => {
-    hideLoader();
+  (error: AxiosError) => {
+    const config = (error.config || {}) as any;
+    const shouldShow = config.showLoader !== false;
+    if (shouldShow) {
+      activeRequests = Math.max(0, activeRequests - 1);
+      if (activeRequests === 0) hideLoader();
+    }
     return Promise.reject(error);
   }
 );
