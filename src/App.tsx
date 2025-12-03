fffdf46ddba6ffc2514ee6879ab3f1e551775ecf
Divyansh <divyanshbfc@gmail.com>
2025-12-03 16:28:21 +0530

    what is mf

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import TermsAndConditions from "./components/TermsAndConditions";
--- src/pages/mfSavings/What-is-mf-savings.tsx
+++ src/pages/mfSavings/What-is-mf-savings.tsx
@@ -1,7 +1,8 @@
 // import correct from "../../assets/img/correct.png";
 // import group from "../../assets/img/Group.png";
+import arrow from "../../assets/img/mf_savings/arrow.png";
 import successImg from "../../assets/img/bg-image/successImg.png";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import MyNavbar from "../../components/Navbar";
 import mfSaving from "../../assets/img/mf_savings/mfSaving.svg";
 import mf_Investing from "../../assets/img/mf_savings/mfInvesting.svg";
@@ -10,6 +11,43 @@ import mfMoney from "../../assets/img/mf_savings/mfMoney.svg";
 const WhatIsMfSavings = () => {
   return (
     <>
+      <style>
+        {`
+          /* Place dots slightly above button */
+          #mfSlider .carousel-indicators {
+            bottom: 80px;
+          }
+
+          /* Hide prev arrow */
+          #mfSlider .carousel-control-prev {
+            display: none !important;
+          }
+
+          /* Bottom centered NEXT button */
+          #mfSlider .carousel-control-next {
+            width: auto;
+            top: auto;
+            bottom: 20px;
+            left: 50%;
+            right: auto;
+            transform: translateX(-50%);
+            opacity: 1 !important;
+          }
+
+          /* Remove default Bootstrap arrow icon */
+          .carousel-control-next-icon {
+            display: none !important;
+          }
+
+          /* Style for your arrow image */
+          .next-arrow-img {
+            width: 70px;  
+            height: 70px;
+            object-fit: contain;
+          }
+        `}
+      </style>
+
       <MyNavbar />
 
       <div className="container mt-4">
@@ -18,15 +56,14 @@ const WhatIsMfSavings = () => {
             <h5>What is MF Savings Account?</h5>
           </div>
 
-          {/* Bootstrap Carousel */}
           <div className="col-lg-8 col-sm-12 mt-4">
             <div
               id="mfSlider"
               className="carousel slide"
-              data-bs-interval="3000"
-              data-bs-ride="carousel"
+              data-bs-ride="false"
+              data-bs-interval="false"
             >
-              {/* Slider Dots */}
+              {/* Dots */}
               <div className="carousel-indicators">
                 <button
                   type="button"
@@ -46,9 +83,9 @@ const WhatIsMfSavings = () => {
                 ></button>
               </div>
 
-              {/* Slider Items */}
+              {/* Slides */}
               <div className="carousel-inner">
-                {/* SLIDE 1 */}
+                {/* Slide 1 */}
                 <div className="carousel-item active">
                   <div
                     className="text-white text-center rounded-4 shadow-lg p-4"
@@ -61,28 +98,24 @@ const WhatIsMfSavings = () => {
                   >
                     <img
                       src={mfSaving}
-                      alt="Slide1"
                       className="img-fluid mt-4"
-                      height={80}
                       width={210}
+                      alt=""
                     />
-
                     <h3 className="mt-3">
                       Your Savings Account is Idle.
-                      <br /> Make it Work.
+                      <br />
+                      Make it Work.
                     </h3>
-
                     <p className="text-white mx-lg-5 mx-2 mt-3">
-                      Why earn just ~3.5% in a savings account? Park your surplus
-                      cash in secure mutual funds and aim for potentially higher
-                      returns with the same ease.
+                      Why earn just ~3.5% in a savings account? Park your
+                      surplus cash in secure mutual funds and aim for
+                      potentially higher returns with the same ease.
                     </p>
-
-                   
                   </div>
                 </div>
 
-                {/* SLIDE 2 */}
+                {/* Slide 2 */}
                 <div className="carousel-item">
                   <div
                     className="text-white text-center rounded-4 shadow-lg p-4"
@@ -95,25 +128,20 @@ const WhatIsMfSavings = () => {
                   >
                     <img
                       src={mf_Investing}
-                      alt="Slide2"
                       className="img-fluid mt-4"
-                      height={80}
                       width={210}
+                      alt=""
                     />
-
-                    <h3 className="mt-3">Smart Investing Made Simple</h3>
-
+                    <h3 className="mt-3">Your Surplus, Smarter Earnings</h3>
                     <p className="text-white mx-lg-5 mx-2 mt-3">
-                      MF Savings Accounts allow you to grow your money without
-                      locking it away. Withdraw anytime and still earn better
-                      than a savings account.
+                      We automatically find idle money in your linked bank
+                      account and invest it in Bajaj Finserv Liquid Fund. You
+                      earn more, without the effort of manual investing.
                     </p>
-
-                  
                   </div>
                 </div>
 
-                {/* SLIDE 3 */}
+                {/* Slide 3 */}
                 <div className="carousel-item">
                   <div
                     className="text-white text-center rounded-4 shadow-lg p-4"
@@ -126,46 +154,31 @@ const WhatIsMfSavings = () => {
                   >
                     <img
                       src={mfMoney}
-                      alt="Slide3"
                       className="img-fluid mt-4"
-                      height={80}
                       width={210}
+                      alt=""
                     />
-
-                    <h3 className="mt-3">Let Your Money Work Harder</h3>
-
+                    <h3 className="mt-3">
+                      Earn More, Without <br />
+                      Lock-In.
+                    </h3>
                     <p className="text-white mx-lg-5 mx-2 mt-3">
-                      Keep your spare cash productive. Earn superior returns with
-                      complete liquidity and low risk.
+                      Need cash? Get instant withdrawals* of up to ₹50,000 back
+                      to your bank account anytime. Enjoy better returns than a
+                      savings account, with incredible flexibility.
                     </p>
-
-                    <Link
-                      className="btn btn-light mt-4 logoBlueColor"
-                      to="/dashboard"
-                    >
-                      Back to Home
-                    </Link>
                   </div>
                 </div>
               </div>
 
-              {/* Prev / Next Arrows */}
-              <button
-                className="carousel-control-prev"
-                type="button"
-                data-bs-target="#mfSlider"
-                data-bs-slide="prev"
-              >
-                <span className="carousel-control-prev-icon"></span>
-              </button>
-
+              {/* CUSTOM IMAGE BUTTON */}
               <button
                 className="carousel-control-next"
                 type="button"
                 data-bs-target="#mfSlider"
                 data-bs-slide="next"
               >
-                <span className="carousel-control-next-icon"></span>
+                <img src={arrow} alt="Next" className="next-arrow-img" />
               </button>
             </div>
           </div>
