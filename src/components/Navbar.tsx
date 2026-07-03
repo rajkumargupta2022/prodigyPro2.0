7cc067476010f02e731fffb21d8aa7fae6c64f7b
rajkumargupta2022 <rajkumarbfcsofttech@gmail.com>
2026-07-03 10:23:26 +0530

    rename of ai-assitant folder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { fetchAdminUser } from "../services/user/adminUser";
 import { renderAdminAvatar } from "../pages/re-used-html/avtar";
-import ChatBoatUi from "../AI-boat/Chat-boat-ui";
+import ChatBoatUi from "../AI-assistant/Chat-boat-ui";
 
 const MyNavbar = () => {
   const location = useLocation();
